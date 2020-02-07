f437c02d365bdfc652e6743bf9af677d018a7795
José Antonio <jocoara1213@gmail.com>
2020-02-08 00:02:20 +0100

    Sesion1B terminada

--- Sesion1B/src/agentes/AgenteEsqueleto.java
+++ Sesion1B/src/agentes/AgenteEsqueleto.java
@@ -5,10 +5,31 @@
  */
 package agentes;
 
+import jade.core.Agent;
+import jade.wrapper.ControllerException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author José Antonio
  */
-public class AgenteEsqueleto {
+public class AgenteEsqueleto extends Agent{
+
+    @Override
+    protected void setup() {
+        try {
+            System.out.println("Hola amigos, soy "+this.getLocalName()+". Acabo de iniciar mi ejecución y estoy en "+this.getContainerController().getContainerName());
+        } catch (ControllerException ex) {
+            Logger.getLogger(AgenteEsqueleto.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    protected void takeDown() {
+         System.out.println("Finaliza la ejecucion del agente: "+this.getLocalName());
+    }
+    
+    
     
 }
